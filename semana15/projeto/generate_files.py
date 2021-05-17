7d637e558f88f1c9a2981353b4774f3e28ac2e3b
Matheus Altoe <matheus.altoe@nappsolutions.com>
2021-05-17 15:14:30 -0300

    Resolucao Semana 15

--- semana15/projeto/generate_files.py
+++ semana15/projeto/generate_files.py
@@ -1,6 +1,47 @@
-import pandas as pd 
+import csv
+from collections import defaultdict
 
-df = pd.read_csv('candidatura.csv', low_memory=False)
 
-for year, value in df.groupby('ano_eleicao'):
-    value.to_csv(f'eleicao_{year}.csv', index=False)
+class ProjetoLargeCSV:
+    def __init__(self):
+        self.anos = list()
+        self.header = self.get_header_years()
+
+    def __repr__(self):
+        return 'Projeto para dividir um csv em arquivos menores'
+
+    def get_header_years(self):
+        with open('candidatura.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            header = list(next(reader).keys())
+            for row in reader:
+                if row.get('ano_eleicao') not in self.anos:
+                    self.anos.append(row.get('ano_eleicao'))
+            return header
+
+
+    def process_data(self, ano):
+        with open('candidatura.csv', 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if row[0] == ano:
+                    anos_agrupados[ano].append(row)
+                yield row
+
+
+client = ProjetoLargeCSV()
+for ano in client.anos:
+    anos_agrupados = defaultdict(list)
+    for i, row in enumerate(client.process_data(ano)):
+        pass
+
+    for ano, value in anos_agrupados.items():
+        valores = list()
+        print(ano)
+        for elen in value:
+            valores.append(elen)
+        with open(f'eleicao_{ano}.csv', 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(client.header)
+            for l in valores:
+                writer.writerow(l)
