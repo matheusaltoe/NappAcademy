225a654b70dc8e0428b551057bddef2bf9606980
Matheus Altoe <matheus.altoe@nappsolutions.com>
2021-06-04 13:53:32 -0300

    Resolucao Semana 18

--- semana18/projeto/encontrar_palavras.py
+++ semana18/projeto/encontrar_palavras.py
@@ -1,24 +1,41 @@
 import os
+import re
 import glob
 
-def palavra_no_arquivo(palavra, arquivo):
-    with open(arquivo, 'r') as f:
-        for line in f:
-            return palavra in line
-    return False
+class EncontrarPalavras:
+    def __init__(self):
+        self.tipos_arquivos = ['**/*.txt', '**/*.csv']
 
-def todos_arquivos_txt():
-    looking_for = '**/*.txt'
-    matched = glob.glob(looking_for, recursive=True)
-    return matched
+    def palavra_no_arquivo(self, palavra, arquivo, ignore_case):
+        ignore_case =  re.IGNORECASE if ignore_case else 0
+        with open(arquivo, 'r') as f:
+            for line in f:
+                match = re.search(palavra, line, flags=ignore_case)
+                if match:
+                    return palavra
+        return False
 
-def encontrar_palavra(palavra):
-    encontrado_em = []
-    arquivos = todos_arquivos_txt()
-    for arquivo in arquivos:
-        if palavra_no_arquivo(palavra, arquivo):
-            encontrado_em.append(arquivo)
-    return encontrado_em
+    def todos_arquivos_txt(self):
+        looking_for = '**/*.txt'
+        matched = glob.glob(looking_for, recursive=True)
+        return matched
 
-busca_napp1 = encontrar_palavra('napp')
-busca_napp2 = encontrar_palavra('NaPp')
+    def todos_arquivos_definidos(self):
+        matched = []
+        for looking_for in self.tipos_arquivos:
+            matched.extend(glob.glob(looking_for, recursive=True))
+        return matched    
+
+    def encontrar_palavra(self, palavra, ignore_case=False):
+        encontrado_em = []
+        arquivos = self.todos_arquivos_definidos()
+        for arquivo in arquivos:
+            if self.palavra_no_arquivo(palavra, arquivo, ignore_case):
+                encontrado_em.append(arquivo)
+        return encontrado_em
+
+client = EncontrarPalavras()
+busca_napp1 = client.encontrar_palavra('napp', ignore_case=False)
+busca_napp2 = client.encontrar_palavra('NaPp', ignore_case=True)
+
+print(busca_napp1)
